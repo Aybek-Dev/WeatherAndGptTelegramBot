49186338825e2ec65b85862ee7ec46a1bc315e08
Aybek-Sultan04 <sultanov.aybek18@gmail.com>
2023-12-31 00:52:53 +0300

    create contum exception

---
+++ WeatherBotForTg/Exceptions/ExceptionHTTPResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace WeatherBotForTg.Exceptions
+{
+    public class ExceptionHTTPResponse : Exception
+    {   
+        public ExceptionHTTPResponse(string message)
+        : base(message)
+        {
+        }
+    }
+}
