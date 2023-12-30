756e33508b1e60a0eb93ce5128e18abf4e750593
Aybek-Sultan04 <sultanov.aybek18@gmail.com>
2023-12-31 00:52:01 +0300

    add into folder class user

---
+++ WeatherBotForTg/Model/User.cs
@@ -0,0 +1,13 @@
+﻿namespace WeatherBotForTg.Model
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public string? UserName { get; set; }
+        public string FirstName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public TimeOnly SendWeatherTime { get; set; }
+
+    }
+}
